69c278a196b353888987688d23307c1aacadc06e
ErezChamilevsky <erez284@gmail.com>
2026-09-14 13:05:45 +0300

    finished

--- RDMATransport.cpp
+++ RDMATransport.cpp
@@ -8,61 +8,78 @@
 #include <memory>
 #include <chrono>
 
-class RDMATransport : public DTransport {
-    private:
-        int fd = -1;
-        struct stat sb;
-        char* mapped = nullptr;
-    
-    public:
-        void init() {
-            fd = open(target, O_RDWR);
-            if (fd == -1)
-            {
-                std::perror("open");
-                return;
-            }
-            struct stat test;
-            sb = test;
+class RDMATransport : public DTransport
+{
+private:
+    int fd = -1;
+    struct stat sb;
+    char *mapped = nullptr;
 
-            if (fstat(fd, &sb) == -1)
-            {
-                std::perror("fstat");
-                close(fd);
-                return;
-            }
+public:
+    void init()
+    {
+        struct stat source_sb;
+        if (stat(source, &source_sb) == -1)
+        {
+            std::perror("stat source");
+            return;
+        }
+        sb = source_sb;
 
-            mapped = static_cast<char *>(mmap(nullptr, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
-            if (mapped == MAP_FAILED)
-            {
-                std::perror("mmap");
-                close(fd);
-                return;
-            }
+        fd = open(target, O_RDWR | O_CREAT, 0644);
+        if (fd == -1)
+        {
+            std::perror("open target");
+            return;
         }
 
-        void send() {
-            std::ifstream fileToSend(source, std::ios::binary); 
-            if (!fileToSend.is_open())
-            {
-                std::perror("fileToSend open");
-                return;
-            }
-
-            fileToSend.read(mapped, sb.st_size);
-
-            fileToSend.close();
+        if (ftruncate(fd, sb.st_size) == -1)
+        {
+            std::perror("ftruncate");
+            close(fd);
+            fd = -1;
+            return;
         }
 
-        int recieve(){
-            
+        mapped = static_cast<char *>(mmap(nullptr, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+        if (mapped == MAP_FAILED)
+        {
+            std::perror("mmap");
+            close(fd);
+            fd = -1;
+            return;
+        }
+    }
+
+    void send()
+    {
+        std::ifstream fileToSend(source, std::ios::binary);
+        if (!fileToSend.is_open())
+        {
+            std::perror("fileToSend open");
+            return;
         }
 
-        void transportClose()
+        fileToSend.read(mapped, sb.st_size);
+        fileToSend.close();
+    }
+
+    int recieve()
+    {
+        return 0;
+    }
+
+    void transportClose()
+    {
+        if (mapped != MAP_FAILED && mapped != nullptr)
         {
             munmap(mapped, sb.st_size);
+        }
+        if (fd != -1)
+        {
             close(fd);
         }
+    }
 };
 
 int main()
